2c5449e6f6fe0c121ec1fc3ac90cbccf924e5463
Jasu <Jasu5511246@gmail.com>
2025-07-30 21:57:07 +0530

    landing-page-completd

--- scrpit/landing.js
+++ scrpit/landing.js
@@ -1,17 +1,20 @@
 let job_seekers = document.querySelector(".job-seekers");
 let employer = document.querySelector(".job-provider");
 
-job_seekers.addEventListener("click", () =>{
+job_seekers.style.backgroundColor = "#0384fc";
+job_seekers.style.color = "#f5f6f7"
+
+job_seekers.addEventListener("click", () => {
     console.log("Job Seekers Section");
     document.querySelector("#job-seekers").style.display = "block";
     document.querySelector("#employers").style.display = "none";
     job_seekers.style.backgroundColor = "#0384fc";
-    job_seekers.style.color ="#f5f6f7"
+    job_seekers.style.color = "#f5f6f7"
     employer.style.backgroundColor = "#f5f6f7";
     employer.style.color = "#0384fc";
-} )
+})
 
-employer.addEventListener("click", () =>{
+employer.addEventListener("click", () => {
     console.log("Employers Section");
     document.querySelector("#job-seekers").style.display = "none";
     document.querySelector("#employers").style.display = "block";
@@ -19,4 +22,4 @@ employer.addEventListener("click", () =>{
     employer.style.color = "#f5f6f7";
     job_seekers.style.backgroundColor = "#f5f6f7";
     job_seekers.style.color = "#0384fc";
-} )
+})
