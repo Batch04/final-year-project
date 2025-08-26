f42ec46d1320533e70e7110a1c34adde6c2c31f4
Vyshnavi <Vyshnavi1234567@gmail.com>
2025-08-26 23:10:17 +0530

    added_files

---
+++ backend/register_seeker.php
@@ -0,0 +1,25 @@
+<?php
+include 'connect.php';
+include 'dbcreate.php';
+include 'tables.php';
+if(isset($_POST['submit'])){
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $pass=$_POST['pass'];
+    $hash_pass=password_hash($pass,PASSWORD_DEFAULT);
+    $phone=$_POST['phone'];
+    $age=$_POST['age'];
+    $skills=$_POST['skills'];
+    $sql="INSERT INTO seekers(name,email,password,phone,age,skills)
+                VALUES('$name','$email','$hash_pass','$phone','$age','$skilss')";
+    $res=mysqli_query($con,$sql);
+    if($res){
+        echo "1 row inserted successfully";
+    }
+    else{
+        echo "Error";
+    }
+
+}
+
+?>
---
+++ backend/tables.php
@@ -0,0 +1,37 @@
+<?php
+$con=mysqli_connect("localhost","root","","partconnectdb");
+if(!$con){
+    echo "connection error";
+}
+echo "connected";
+$sql="CREATE TABLE IF NOT EXISTS seekers(
+id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(50) NOT NULL,
+email VARCHAR(100) NOT NULL,
+password VARCHAR(255) NOT NULL,
+phone INT(10) NOT NULL ,
+age INT(5) NOT NULL,
+skills TEXT NOT NULL,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
+if(mysqli_query($con,$sql)){
+    echo "seeker table created";
+}
+else{
+    echo "creation error";
+}
+$sql2="CREATE TABLE  IF NOT EXISTS providerr(
+id INT AUTO_INCREMENT PRIMARY KEY,
+company_name VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL UNIQUE,
+password VARCHAR(255) NOT NULL,
+address VARCHAR(255) NOT NULL,
+location VARCHAR(255) NOT NULL,
+contact_number VARCHAR(20),
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)";
+if(mysqli_query($con,$sql2)){
+    echo "provider table created";
+}
+else{
+    echo "error";
+}
+?>
